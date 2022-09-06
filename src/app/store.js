8f005e9dc56ef13c69a5bcb31427c948d3d2ea81
Mikael <dev.kov.mtl@gmail.com>
2022-09-06 14:33:24 -0400

    Post reducer slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
+import storeReducer from "../features/posts/postsSlice";
 
 export const store = configureStore({
   reducer: {
     // reducers
     counter: counterReducer,
+    posts: storeReducer,
   },
 });
