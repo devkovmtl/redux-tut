79ba81f1a4eb6a3defe4e25ebd404e63a8462289
Mikael <dev.kov.mtl@gmail.com>
2022-09-07 15:30:54 -0400

    Store update and userSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import storeReducer from "../features/posts/postsSlice";
+import usersReducer from "../features/users/usersSlice";
 
 export const store = configureStore({
   reducer: {
     // reducers
     counter: counterReducer,
     posts: storeReducer,
+    users: usersReducer,
   },
 });
