88fa6c80ffd46c13db9b38fe483a7565bd7d3050
Mikael <dev.kov.mtl@gmail.com>
2022-09-13 14:29:51 -0400

    Update Posts List

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -8,13 +8,15 @@ import {
   selectPostByIds,
 } from "./postsSlice";
 import PostsExcerpt from "./PostsExcerpt";
+import { useGetPostsQuery } from "./postsSlice";
 
 const PostsList = () => {
+  const { isLoading, isSuccess, isError, error } = useGetPostsQuery();
   // const dispatch = useDispatch();
   // const posts = useSelector(selectAllPosts);
   const orderedPostIds = useSelector(selectPostByIds);
   const postsStatus = useSelector(getPostsStatus);
-  const error = useSelector(getPostsErrror);
+  // const error = useSelector(getPostsErrror);
 
   // useEffect(() => {
   //   if (postsStatus === "idle") {
@@ -32,9 +34,11 @@ const PostsList = () => {
 
   let content;
 
-  if (postsStatus === "loading") {
+  // if (postsStatus === "loading") {
+  if (isLoading) {
     content = <p>"Loading..."</p>;
-  } else if (postsStatus === "succeeded") {
+    // } else if (postsStatus === "succeeded") {
+  } else if (isSuccess) {
     // const orderedPosts = posts
     //   .slice()
     //   .sort((a, b) => b.data.localeCompare(a.data));
@@ -42,7 +46,8 @@ const PostsList = () => {
     content = orderedPostIds.map((postId) => (
       <PostsExcerpt key={postId.id} post={postId} />
     ));
-  } else if (postsStatus === "failed") {
+    // } else if (postsStatus === "failed") {
+  } else if (isError) {
     content = <p>{error}</p>;
   }
 
